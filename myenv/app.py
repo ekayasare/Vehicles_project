813d167373ce951af4ab3c5675e44a0799c405c6
Ekay <info.emmanuelasare@gmail.com>
2024-10-27 22:24:50 -0700

    Added Streamlit

---
+++ myenv/app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import plotly.express as px
+
+st.header('Car Sales Advertisements')
+st.write('Filter the data below to see the ads by price')
+
